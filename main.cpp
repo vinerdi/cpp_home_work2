70f510eda398b365f3f9ae667ddb713c0a2f219c
vinerdi <vladiktaran2008@gmail.com>
2024-11-12 22:04:46 +0200

    вщту

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 template <typename T>
+class Node {
+public:
+    T data;
+    Node* next;
+    Node* prev;
+
+    Node(T value) : data(value), next(nullptr), prev(nullptr) {}
+};
+
+template <typename T>
 class DoublyLinkedList {
 private:
     Node<T>* head;
@@ -8,18 +18,15 @@ public:
     DoublyLinkedList() : head(nullptr), tail(nullptr) {}
 
     // Клонування списку
-    Node<T>* clone() const {
-        if (head == nullptr) return nullptr;
-        Node<T>* newHead = new Node<T>(head->data);
-        Node<T>* current = head->next;
-        Node<T>* newCurrent = newHead;
+    DoublyLinkedList<T> clone() const {
+        DoublyLinkedList<T> newList;
+        if (head == nullptr) return newList;
+        Node<T>* current = head;
         while (current != nullptr) {
-            newCurrent->next = new Node<T>(current->data);
-            newCurrent->next->prev = newCurrent;
-            newCurrent = newCurrent->next;
+            newList.append(current->data);
             current = current->next;
         }
-        return newHead;
+        return newList;
     }
 
     // Перевантаження оператора +
@@ -77,18 +84,18 @@ public:
 };
 
 int main() {
-	DoublyLinkedList<int> list1;
-	list1.append(1);
-	list1.append(2);
-	list1.append(3);
+    DoublyLinkedList<int> list1;
+    list1.append(1);
+    list1.append(2);
+    list1.append(3);
 
-	DoublyLinkedList<int> list2;
-	list2.append(2);
-	list2.append(3);
-	list2.append(4);
+    DoublyLinkedList<int> list2;
+    list2.append(2);
+    list2.append(3);
+    list2.append(4);
 
-	DoublyLinkedList<int> list3 = list1 + list2;
-	DoublyLinkedList<int> list4 = list1 * list2;
+    DoublyLinkedList<int> list3 = list1 + list2;
+    DoublyLinkedList<int> list4 = list1 * list2;
 
-	return 0;
+    return 0;
 }
